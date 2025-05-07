8b69d0091b7715fcffa879778c8682c6300ef24b
Juan Pablo Del Muro <jpdelmuro@gmail.com>
2025-05-07 13:55:26 -0600

    inventario y stock

--- BACKEND/models/producto.js
+++ BACKEND/models/producto.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
-const ProductoSchema = new mongoose.Schema({
+const productoSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  quantity: { type: Number, required: true },
-  category: { type: String, required: true },
+  quantity: { type: Number, default: 1 },
+  category: { type: String },
   type: { type: String, enum: ['shoppingList', 'inventory'], required: true },
+  price: { type: String },
+  barcode: { type: String },
   owners: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 });
 
-module.exports = mongoose.model('Producto', ProductoSchema);
+module.exports = mongoose.model('Producto', productoSchema);
