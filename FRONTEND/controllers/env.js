70b25d5b6104e8570d32e17900aeb9cd254c55b6
Ikram <luisa.zaldivar@iteso.mx>
2025-05-07 07:28:25 -0600

    localhost fix

--- FRONTEND/controllers/env.js
+++ FRONTEND/controllers/env.js
@@ -1,21 +1,19 @@
-// Detectar si estamos en desarrollo local o en producción
-const isLocalhost = window.location.hostname === "localhost";
+const isLocalhost = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 
-// Backend local o Render
 export const backend_url = isLocalhost
   ? "http://localhost:3000/"
   : "https://proyectoweb-k36n.onrender.com/";
 
-// Frontend local o GitHub Pages
 export const frontend_url = isLocalhost
-  ? "http://localhost:5500/FRONTEND/views/" // cambia el puerto si usas otro
+  ? "http://127.0.0.1:5500/FRONTEND/views/"
   : "https://jpdelmuro.github.io/ProyectoWeb/FRONTEND/views/";
+
 
 // Validar sesión del usuario 
 export function validateLogin(redirectTo = "login.html") {
   const user = sessionStorage.getItem("user");
   if (!user) {
     alert("Favor de iniciar sesión");
-    window.location.href = redirectTo;
+    window.location.href = frontend_url + redirectTo;
   }
 }
