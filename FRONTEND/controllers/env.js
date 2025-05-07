b00ec0b88ffb40f1c58602a0d283088268cbb695
Juan Pablo Del Muro <jpdelmuro@gmail.com>
2025-05-07 11:57:32 -0600

    filtrado solucionado

--- FRONTEND/controllers/env.js
+++ FRONTEND/controllers/env.js
@@ -1,10 +1,7 @@
 const hostname = window.location.hostname;
 const isLocalhost = hostname === "localhost" || hostname === "127.0.0.1";
 
-<<<<<<< HEAD
 // Usa la IP base detectada
-=======
->>>>>>> 7c6c41e25a609cef1ee65aefda92311cf1266d0e
 const baseFrontend = `${window.location.origin}/FRONTEND/views/`;
 
 export const backend_url = isLocalhost
@@ -14,6 +11,9 @@ export const backend_url = isLocalhost
 export const frontend_url = isLocalhost
   ? baseFrontend
   : "https://jpdelmuro.github.io/ProyectoWeb/FRONTEND/views/";
+
+
+
 
 // Validar sesión del usuario 
 export function validateLogin(redirectTo = "login.html") {
